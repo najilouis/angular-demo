cdaf2d9577a3f900e41f2bfb2281d6443c482f7d
Naji Louis <naji@anghami.com>
2018-01-05 21:06:01 +0200

    dynamic scss variables

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public themeMode: any = 'light';
+  private body: any = document.getElementsByTagName('body')[0];
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    // this.body = document.getElementsByTagName('body')[0]; 
+    this.body.classList.add(this.themeMode); 
   }
 
+  toggle(){
+    if(this.themeMode == 'light'){
+      this.themeMode = 'dark';
+    }
+    else{
+      this.themeMode = 'light';
+    }
+    this.body = document.getElementsByTagName('body')[0];
+    this.body.classList.remove("dark");
+    this.body.classList.remove("light");
+    this.body.classList.add(this.themeMode);
+
+  }
 }
